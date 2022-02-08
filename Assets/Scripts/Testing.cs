52b1c4c5be5d8c249664fc0d5fc16b37ab6e0c86
Julien Lemaire <julien.lemaire@sticmac.fr>
2022-02-08 16:37:05 +0100

    Fin de la formation

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -2,18 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Créer une classe pour une pomme
-// une pomme a une couleur et un poids (en grammes, environ 100g)
-// => créer plusieurs pommes et afficher leur couleur et leur poids
-// Indication : on peut tout à fait créer une méthode qui écrit directement dans la console
+public abstract class Vehicule { // classe abstraite = on ne pourra jamais faire new Vehicule()
+    protected int roues;
 
-// Pensez à remplacer Testing par le nom de votre script pour que ça fonctionne
+    public Vehicule(int roues) {
+        this.roues = roues;
+    }
+
+    public abstract void Afficher();
+}
+
+public class Voiture : Vehicule {
+    public Voiture() : base(4) {
+    }
+
+    public override void Afficher() {
+        Debug.Log("Ceci est une voiture");
+    }
+
+    public void Klaxonner() {
+
+    }
+}
+
+public class Moto : Vehicule {
+    public Moto() : base(2) {
+
+    }
+
+    public override void Afficher() {
+        Debug.Log("Ceci est une moto");
+    }
+}
+
 public class Testing : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // Créer les pommes ici et afficher leurs propriétés
+        // Polymorphisme
+        Vehicule vehicule1 = new Voiture();
+        vehicule1.Afficher(); // Ceci est une voiture
+        // On ne peut pas écrire « vehicule1.Klaxonner(); » car Vehicule ne contient pas la méthode Klaxonner
+
+        Vehicule vehicule2 = new Moto();
+        vehicule2.Afficher();
+
+        // Grâce au polymorphisme, on peut stocker les classes filles dans un tableau du type de la classe mère
+        Vehicule[] vehicules = {
+            new Voiture(),
+            new Moto(),
+            new Voiture(),
+            new Voiture()
+        };
+
+        for (int i = 0 ; i < vehicules.Length ; i++) {
+            vehicules[i].Afficher();
+        }
     }
 
     // Update is called once per frame
