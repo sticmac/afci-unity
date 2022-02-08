4d76c0045f984c98a08093c50bf60c47a2a724b5
Julien Lemaire <julien.lemaire@sticmac.fr>
2022-02-08 11:22:44 +0100

    Début de l'exercice des pommes

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -2,22 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Créer une classe pour une pomme
+// une pomme a une couleur et un poids (en grammes, environ 100g)
+// => créer plusieurs pommes et afficher leur couleur et leur poids
+// Indication : on peut tout à fait créer une méthode qui écrit directement dans la console
+
+// Pensez à remplacer Testing par le nom de votre script pour que ça fonctionne
 public class Testing : MonoBehaviour
 {
-    [SerializeField] string text = "Voix ambigue d'un cœur qui au zéphyr préfère les jattes de kiwi.";
-
-    // Écrire la fonction WordCount qui compte le nombre de mots dans une variable string donnée
-    // Indice : les variables string ont une fonction « Split » divise en un tableau en fonction d'un caractère donnéez
-    // Exemple : text.Split(' ') => {"Hello", "world!"}
-    int WordCount(string t) {
-        string[] words = t.Split(' ');
-        return words.Length;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(WordCount(text));
+        // Créer les pommes ici et afficher leurs propriétés
     }
 
     // Update is called once per frame
